8b3e31514199ebeddbd8dd5fff82985af470257b
Grace Lavin <gcl5087@psu.edu>
2020-09-01 13:15:23 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <readline/readline.h> 
+
+int main(void) {
+  char *name = readline("Enter your name: ");
+  printf("Hello, %s!\n", name);
+  return 0;
+
+
+
+  celsiusV = input("Enter temperature in celsius: ")
+
+
+celsiusV = float(celsiusV)
+
+fahrV = (1.8 *(celsiusV) + 32)
+
+print( str(celsiusV) + "° in Celsius is equivalent to " + str(fahrV) + "° Fahrenheit.")
+
+
+
+  
+}
